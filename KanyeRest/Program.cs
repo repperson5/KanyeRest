2623ecc52f2edd78a35d07e79e89cc9b6c7bbd9b
Rhea Epperson <re.epperson2@gmail.com>
2023-09-20 15:20:24 -0400

    exercise complete

---
+++ KanyeRest/Program.cs
@@ -0,0 +1,23 @@
+﻿using KanyeRest;
+using Newtonsoft.Json.Linq;
+
+//var client = new HttpClient();
+
+//var kanyeURL = "https://api.kanye.rest/";
+
+//var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+//var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+//var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+//var ronResponse = client.GetStringAsync(ronURL).Result;
+
+//var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+for (int i = 0; i < 5; i++)
+{ 
+Console.WriteLine($"Kanye says: \"{QuoteGenerator.GetYeQuote()}\"");
+
+Console.WriteLine($"Ron says : {QuoteGenerator.GetRonQuote()}");
+}
---
+++ KanyeRest/QuoteGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeRest
+{
+	public class QuoteGenerator
+	{
+		public static string GetYeQuote()
+		{
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            //Return and store JSON reponse from API
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            //use Newtonsoft.JSON to parse our JSON response
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public static string GetRonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            //Call API and store response
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            //Parse JSON with Newtonsoft.JSON
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+
+        }
+
+
+    }
+}
+
